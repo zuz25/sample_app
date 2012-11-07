1006cdb08833693b9f41c25943bab695c184a8fe
Bryan Hicks <zuz25@byu.net>
2012-11-06 21:05:13 -0600

    Add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,48 +2,57 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+let(:base_title) { "BH Sample App" }
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+        it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
-
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
-
 
   describe "About page" do
-    it "should have the content 'About'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
+  describe "Contact page" do
 
-it "should have the right title" do
-  visit '/static_pages/home'
-  page.should have_selector('title',
-                    :text => "BH Sample App | Home")
-end
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
 
-it "should have the right title" do
-  visit '/static_pages/help'
-  page.should have_selector('title',
-                    :text => "BH Sample App | Help")
-end
-
-it "should have the right title" do
-  visit '/static_pages/about'
-  page.should have_selector('title',
-                    :text => "BH Sample App | About")
-end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
 end
